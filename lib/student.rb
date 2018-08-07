ad3988dbcb8e334e51195b549510e1ecd2c66cf9
Vicki Hamm <vicki.cello@gmail.com>
2018-08-07 19:59:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,9 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+  attr_writer :id
+
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
