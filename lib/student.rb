064d2f0a5f3acaa45ee04e8b1ed3872e6c2d2a59
Vicki Hamm <vicki.cello@gmail.com>
2018-08-07 20:02:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   attr_accessor :name, :grade
   attr_writer :id
 
-  def initialize(name, grade, id = nil)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
