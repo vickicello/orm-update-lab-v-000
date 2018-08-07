d16993d3d3aa5689d82731ce37e112c6e5cb74d3
Vicki Hamm <vicki.cello@gmail.com>
2018-08-07 20:03:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,11 +23,23 @@ class Student
   end
 
   def self.drop_table
-
+    sql =  <<-SQL
+      DROP TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 
   def save
+    sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
 
+      DB[:conn].execute(sql, self.name, self.grade)
   end
 
   def self.create
